c63eab8ae5b3e6f21aad6a9f0ac4ffb44b4756f9
Leland Curtis <lelandcurtis88@gmail.com>
2022-02-26 11:30:10 -0700

    Create facade and service classes. Stub tests.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,5 @@
 class DashboardsController < ApplicationController
   def index
+    @listings = ListingFacade.get_listings(:q)
   end
 end
--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -11,17 +11,18 @@ RSpec.describe 'dashboard page' do
     describe 'model filters' do
       let!(:make_options_1) { ["A", "B", "C"] }
       let!(:make_options_2) { ["A", "B", "C", "D", "E"] }
+      let!(:listings) { build_list(:listing, 10) }
 
-      it "has model dropdowns that match available models in database" do
+      it "has model dropdowns that match available models in retreived listings" do
 
-        allow(ListingService).to receive(:get_makes).and_return({data: options_1})
+        #allow(ListingService).to receive(:get_makes).and_return({data: make_options_1})
+
+        allow(ListingFacade).to (receive(:get_listings).and_return(listings))
 
         visit '/'
         within 'div.filters' do
-          makes = ListingService.get_makes
-
-          makes.each do |make|
-            select make, from: "Make"
+          listings.each do |listing|
+            select listing.make, from: "Make"
           end
         end
       end
