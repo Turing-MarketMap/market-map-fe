0f30b58ada04c1838a608be9213b5d88291ba849
Tim Roeder <timothyjroeder@gmail.com>
2022-02-27 10:04:43 -0700

    Routes: add dashboard#index, user#show and listings#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'dashboard#index'
+  get '/profile', to: 'user#show'
+  resources :listings, only: %i[show]
 end
