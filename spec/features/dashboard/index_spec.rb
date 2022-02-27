272a8d81a6f0f1e8c15a732da76e6326266b6b4f
Leland Curtis <lelandcurtis88@gmail.com>
2022-02-26 17:39:20 -0700

    Create helper methods for finding make and model from listings object.

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'dashboard page' do
-  let!(:listings) { build_list(:listing, 10) }
+  let!(:listings) { build(:listings, listing_count: 10) }
+  let!(:listings_2) { build(:listings, listing_count: 10) }
 
 
   it "is the root page" do
-    allow(ListingFacade).to (receive(:get_listings).and_return(listings))
+    allow(ListingFacade).to receive(:get_listings).and_return(listings)
     visit '/'
     expect(page).to have_content("Market Map")
   end
@@ -19,14 +20,31 @@ RSpec.describe 'dashboard page' do
 
         #allow(ListingService).to receive(:get_makes).and_return({data: make_options_1})
         allow(ListingFacade).to receive(:get_listings).and_return(listings)
-        allow(Listing).to receive(:all_makes).and_return(make_options_2)
-
+        allow(listings).to receive(:all_makes).and_return(make_options_2)
 
         visit '/'
-        save_and_open_page
+
         within 'div.filters' do
           make_options_2.each do |make|
             select make, from: "Make"
+          end
+        end
+      end
+    end
+
+    describe 'model filters' do
+      it "has model dropdowns that match available models in retreived listings" do
+        make = listings_2.listings.sample(1)[0].make
+        models = listings_2.all_models(make)
+
+        allow(ListingFacade).to receive(:get_listings).and_return(listings_2)
+
+        visit '/'
+
+        within 'div.filters' do
+          select make, from: "Make"
+          models.each do |model|
+            select model, from: "Model"
           end
         end
       end
--- spec/poros/listings_spec.rb
+++ spec/poros/listings_spec.rb
@@ -11,4 +11,99 @@ RSpec.describe 'Listings poro' do
     expect(listings_object.listings).to be_a(Array)
     expect(listings_object.listings.sample(1)[0]).to be_a(Listing)
   end
+
+  describe 'instance methods' do
+    let!(:listing_1) { build(:listing, make: 'Honda', model: 'Civic')}
+    let!(:listing_2) { build(:listing, make: 'Honda', model: 'Accord')}
+    let!(:listing_3) { build(:listing, make: 'Mazda', model: 'Mazda3')}
+
+    let!(:listings_object) { Listings.new([listing_1, listing_2, listing_3]) }
+
+    describe '#all_makes' do
+      it "returns an array of all the available makes in the collection" do
+        expect(listings_object.all_makes).to eq(['Honda', 'Mazda'])
+      end
+    end
+
+    describe '#all_models' do
+      it "returns an array of all of the available models for the chosen make" do
+        expect(listings_object.all_models('Honda')).to be_a(Array)
+        expect(listings_object.all_models('Honda')).to eq(['Accord', 'Civic'])
+        expect(listings_object.all_models('Mazda')).to eq(['Mazda3'])
+      end
+    end
+  end
+
+  describe 'class methods' do
+    describe '.all_makes' do
+      xit "returns a hash of all the available makes in the base model" do
+        expect(Listing.all_makes).to eq(["Acura",
+                                        "airstream",
+                                        "Aston Martin",
+                                        "Audi",
+                                        "Bentley",
+                                        "BMW",
+                                        "Buick",
+                                        "Cadillac",
+                                        "chev truck",
+                                        "Chevrolet",
+                                        "Chrysler",
+                                        "Daewoo",
+                                        "Dodge",
+                                        "dodge tk",
+                                        "dot",
+                                        "Ferrari",
+                                        "FIAT",
+                                        "Fisker",
+                                        "Ford",
+                                        "ford tk",
+                                        "ford truck",
+                                        "Geo",
+                                        "GMC",
+                                        "gmc truck",
+                                        "Honda",
+                                        "HUMMER",
+                                        "Hyundai",
+                                        "hyundai tk",
+                                        "Infiniti",
+                                        "Isuzu",
+                                        "Jaguar",
+                                        "Jeep",
+                                        "Kia",
+                                        "Lamborghini",
+                                        "Land Rover",
+                                        "landrover",
+                                        "Lexus",
+                                        "Lincoln",
+                                        "Lotus",
+                                        "Maserati",
+                                        "Mazda",
+                                        "mazda tk",
+                                        "mercedes",
+                                        "mercedes-b",
+                                        "Mercedes-Benz",
+                                        "Mercury",
+                                        "MINI",
+                                        "Mitsubishi",
+                                        "Nissan",
+                                        "Oldsmobile",
+                                        "plymouth",
+                                        "pontiac",
+                                        "Porsche",
+                                        "Ram",
+                                        "Rolls-Royce",
+                                        "Saab",
+                                        "Saturn",
+                                        "Scion",
+                                        "smart",
+                                        "Subaru",
+                                        "Suzuki",
+                                        "Tesla",
+                                        "Toyota",
+                                        "Volkswagen",
+                                        "Volvo",
+                                        "vw"])
+      end
+    end
+  end
 end
