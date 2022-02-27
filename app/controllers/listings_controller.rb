941a3e4a91c4144c0b10304bdc0438b1e61a519e
Tim Roeder <timothyjroeder@gmail.com>
2022-02-27 10:12:36 -0700

    Cont: add listings#show with private params method

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,26 @@
+class ListingsController < ApplicationController
+  def show
+    @listing = Listing.new(listing_params)
+    @image = ImageFacade.search_for_listing_image(@listing.title, @listing.color)
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:id, 
+                                    :year, 
+                                    :make, 
+                                    :model, 
+                                    :trim, 
+                                    :body, 
+                                    :transmission, 
+                                    :vin, 
+                                    :state, 
+                                    :condition, 
+                                    :odometer, 
+                                    :color, 
+                                    :interior, 
+                                    :selling_price, 
+                                    :title)
+  end
+end
