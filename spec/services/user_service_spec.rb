d593f0417adec3ccc7c269a6e4155f628ad16dfd
Eldridge Turambi <eturambi95@gmail.com>
2022-02-27 17:14:32 -0700

    update test tp use vcr for now

--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'User Service' do
 
-  it 'can get all users' do
+  it 'can get all users', :vcr do
     users =  UserService.all_users#[:data]
-
     expect(users).to be_a(Array)
   end
 end
