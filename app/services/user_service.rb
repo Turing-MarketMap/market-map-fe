7492403de392e2dbd77636d3532dc669847a5c5d
Eldridge Turambi <eturambi95@gmail.com>
2022-02-27 17:13:26 -0700

    Took out api_key for now. Need to configure api key for backend later

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -3,9 +3,7 @@ class UserService
   class << self
 
     def connection
-      Faraday.new(url: "http://localhost:3000/api/v1/") do |f|
-        f.paranns['api_key'] = ENV['user_api_key']
-      end
+      Faraday.new(url: "http://localhost:3000/api/v1/")
       # JSON.parse(response.body, symbolize_names: true)
     end
 
@@ -14,7 +12,9 @@ class UserService
     end
 
     def all_users
-      response = connection.get('/users')
+      response = connection.get('users')
+      json_response = parse_json(response)
+      json_response
     end
   end
 end
