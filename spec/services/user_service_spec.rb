71ae950e2b0475b72e7ca358996103de33a46e30
Eldridge Turambi <eturambi95@gmail.com>
2022-02-28 12:39:47 -0700

    Update test

--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'User Service' do
+  before :each do
 
-  it 'can get all users', :vcr do
-    users =  UserService.all_users#[:data]
-    expect(users).to be_a(Hash)
+  end
+
+  it 'can get all users' do
+
+    users_json = "{\"data\":{\"id\":\"1\",\"type\":\"user\",\"attributes\":{\"first_name\":\"eldridge\",\"last_name\":\"turambi\",\"email\":\"eturambi@gmail.com\"}}}"
+
+    stub_request(:post, "http://localhost:3000/api/v1/users").with(query: { 'user[first_name]' => 'jackson', 'user[last_name]' => 'valdez', 'user[email]' => 'jvaldez@gmail.com'}).to_return(status: 200, body: users_json, headers: {})
+
+    user =  UserService.find_user('eturambi@gmail.com')
+    expect(user[:email]).to eq('eturambi@gmail.com')
   end
 end
