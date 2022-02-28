b3c7aafb04832d94f20653284ed88e7e46076cfb
Eldridge Turambi <eturambi95@gmail.com>
2022-02-28 16:11:29 -0700

    Add: login for user controller to have session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
 
   def index
-    user = UserService.find_user(email)
+    user = UserFacade.find_user(email)
   end
 
   def new
 
+  end
+
+  def login
+    user = UserService.find_user('fake@gmail.com', 'name1', 'last_name1')
+    session[:user_id] = user[:data][:id]
+    redirect_to root_path
   end
 end
