6e9ec80f345764b329c7eb2cbf36f9597bc9f28a
Eldridge Turambi <eturambi95@gmail.com>
2022-02-27 18:08:44 -0700

    Fixed User service to work. It is now a matter of getting the back end users seeded

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -3,7 +3,7 @@ class UserService
   class << self
 
     def connection
-      Faraday.new(url: "http://localhost:3000/api/v1/")
+      Faraday.new(url: "http://localhost:3000/api/v1/") 
       # JSON.parse(response.body, symbolize_names: true)
     end
 
@@ -12,7 +12,7 @@ class UserService
     end
 
     def all_users
-      response = connection.get('users')
+      response = connection.get('/users')
       json_response = parse_json(response)
       json_response
     end
--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe 'User Service' do
 
   it 'can get all users', :vcr do
     users =  UserService.all_users#[:data]
-    expect(users).to be_a(Array)
+    expect(users).to be_a(Hash)
   end
 end
