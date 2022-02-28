83a98cff46378583ae4f10c156d9bd2246a5edec
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-02-28 14:54:59 -0700

    Test: find_user from UserService

--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe 'User Service' do
+RSpec.describe UserService do
   before :each do
 
   end
 
-  it 'can get all users' do
+  it 'can get user from params', :vcr do
 
     users_json = "{\"data\":{\"id\":\"1\",\"type\":\"user\",\"attributes\":{\"first_name\":\"eldridge\",\"last_name\":\"turambi\",\"email\":\"eturambi@gmail.com\"}}}"
 
-    stub_request(:post, "http://localhost:3000/api/v1/users").with(query: { 'user[first_name]' => 'jackson', 'user[last_name]' => 'valdez', 'user[email]' => 'jvaldez@gmail.com'}).to_return(status: 200, body: users_json, headers: {})
+    stub_request(:post, "http://localhost:3000/api/v1/users").with(query: { 'user[email]' => 'eturambi@gmail.com', 'user[first_name]' => 'eldridge', 'user[last_name]' => 'turambi'}).to_return(status: 200, body: users_json, headers: {})
 
-    user =  UserService.find_user('eturambi@gmail.com')
-    expect(user[:email]).to eq('eturambi@gmail.com')
+    user =  UserService.find_user('eturambi@gmail.com', 'eldridge', 'turambi')
+    expect(user[:data][:attributes][:email]).to eq('eturambi@gmail.com')
+    expect(user[:data][:attributes][:first_name]).to eq('eldridge')
+    expect(user[:data][:attributes][:last_name]).to eq('turambi')
   end
 end
