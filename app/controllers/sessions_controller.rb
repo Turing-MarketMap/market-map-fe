e55202c6efec29b1fe42bedeeeb46fab07d8d74f
Tim Roeder <timothyjroeder@gmail.com>
2022-03-02 14:59:59 -0700

    View: add link to root path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,10 @@
 class SessionsController < ApplicationController
   def create
     session[:auth_hash] = request.env['omniauth.auth']
+    current_user
     binding.pry
-    user = UserService.find_user('fake@gmail.com', 'name1', 'last_name1')
+    user = UserService.find_user(@current_user[:email], @current_user[:first_name], @current_user[:last_name])
+    binding.pry
     session[:user_id] = user[:data][:id]
 
     redirect_to root_path
