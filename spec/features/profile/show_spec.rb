c02ae86f3334d74b80f938c81a14c253d3400c83
Croix Kimmel <croixkimmel@gmail.com>
2022-03-02 16:25:29 -0700

    Add user profile show page, happy path testing

--- spec/features/profile/show_spec.rb
+++ spec/features/profile/show_spec.rb
@@ -1,12 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe 'User profile show page' do
+RSpec.describe 'User profile show page', :vcr, type: :feature do
+  before(:each) do
+    OmniAuth.config.test_mode = true
+    auth_hash = Faker::Omniauth.google
+    OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new(auth_hash)
+  end
+
   context 'as a logged in user', :vcr do
     it 'can display information on user' do
+      visit '/'
+      click_link 'Login with Google'
+
       visit '/profile'
-      expect(page).to have_content("Saved listings: ")
+      expect(page).to have_content("My account details:")
+      expect(page).to have_content("Name: Example User")
+      expect(page).to have_content("User ID: 1234")
       expect(page).to have_link("Delete my account")
-      expect(page).to have_content("My account details: ")
+      ### Add tests for saved listings
+      
     end
   end
 end
