0a79efb33b6dad5c86a9f8f5a3177a59ed61d08e
Tim Roeder <timothyjroeder@gmail.com>
2022-03-02 14:43:47 -0700

    Cont: remove #login action and move logic to session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,10 @@
 class SessionsController < ApplicationController
   def create
     session[:auth_hash] = request.env['omniauth.auth']
+    binding.pry
+    user = UserService.find_user('fake@gmail.com', 'name1', 'last_name1')
+    session[:user_id] = user[:data][:id]
+
     redirect_to root_path
   end
 
