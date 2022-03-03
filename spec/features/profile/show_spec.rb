7f87031fdc7ef693419ef25f3b24601890438956
Croix Kimmel <croixkimmel@gmail.com>
2022-03-02 19:37:50 -0700

    Add edge cases, user show page

--- spec/features/profile/show_spec.rb
+++ spec/features/profile/show_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'User profile show page', :vcr, type: :feature do
-  before(:each) do
+
+  context 'as a logged in user', :vcr do
     OmniAuth.config.test_mode = true
     auth_hash = Faker::Omniauth.google
     OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new(auth_hash)
-  end
 
-  context 'as a logged in user', :vcr do
     it 'can display information on user' do
       visit '/'
       click_link 'Login with Google'
@@ -18,7 +17,26 @@ RSpec.describe 'User profile show page', :vcr, type: :feature do
       expect(page).to have_content("User ID: 1234")
       expect(page).to have_link("Delete my account")
       ### Add tests for saved listings
-      
+
+    end
+
+    it 'no longer shows profile page after user logs out' do
+      visit '/'
+      click_link 'Login with Google'
+      click_link 'Logout'
+      visit '/profile'
+      expect(current_path).to eq('/')
+      expect(page).to have_content('Please login to see user profile information')
+
+    end
+  end
+
+  context 'not logged in', :vcr do
+    it 'does not show profile page' do
+      visit '/profile'
+      expect(current_path).to eq('/')
+      expect(page).to have_content('Please login to see user profile information')
+      save_and_open_page
     end
   end
 end
