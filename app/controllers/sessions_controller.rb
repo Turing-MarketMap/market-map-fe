e18b09076a9eac4b52461be37b57a3077b0d7c7e
Tim Roeder <timothyjroeder@gmail.com>
2022-03-02 20:15:59 -0700

    Cont: update #create to include user_id in session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,8 @@ class SessionsController < ApplicationController
   def create
     session[:auth_hash] = request.env['omniauth.auth']
     current_user
-    binding.pry
     user = UserService.find_user(@current_user[:email], @current_user[:first_name], @current_user[:last_name])
-    binding.pry
-    session[:user_id] = user[:data][:id]
+    session[:auth_hash]['info']['user_id'] = user[:data][:id]
 
     redirect_to root_path
   end
