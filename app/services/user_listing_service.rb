0fa20b028a634915d65b02ecac583958935d39c8
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-03 21:20:29 -0700

    Add UserListingService

--- app/services/user_listing_service.rb
+++ app/services/user_listing_service.rb
@@ -1,7 +1,23 @@
 class UserListingService
 
-  def self.create_user_listing(user_id, listing_id)
+  def self.connection
+    Faraday.new(url: ENV['consultancy_be'])
+  end
 
+  def self.parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.create_user_listing(user_id, listing_id)
+    response = connection.post("users/#{user_id}/listings") do |req|
+      req.params['user_id'] = user_id
+      req.params['listing_id'] = listing_id
+    end
+    parse_json(response)
+  end
+
+  def self.delete_user_listing(user_id, listing_id)
+    connection.delete("users/#{user_id}/listings/#{listing_id}")
   end
 
 end
