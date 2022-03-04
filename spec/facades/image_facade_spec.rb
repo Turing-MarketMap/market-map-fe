7df32f2930351d5b2e39f2e6ed546fe504b5f2de
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-03 21:20:19 -0700

    fix vcr error

--- spec/facades/image_facade_spec.rb
+++ spec/facades/image_facade_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe ImageFacade, type: :facade do
   context 'class methods' do
-    describe '::search_for_listing_image' do
-      it 'returns an image object', :vcr do
+    describe '::search_for_listing_image', :vcr do
+      it 'returns an image object' do
         image = ImageFacade.search_for_listing_image('2015 kia sorento', 'blue')
 
         expect(image).to be_a(Image)
