01953a023df1f031638203b553aa02733dd59d53
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-03 21:19:40 -0700

    Add create and destroy actions UserListingsController

--- app/controllers/user_listings_controller.rb
+++ app/controllers/user_listings_controller.rb
@@ -1,3 +1,15 @@
 class UserListingsController < ApplicationController
+  before_action :current_user
 
+  def create
+    UserListingService.create_user_listing(@current_user[:user_id], params[:listing_id])
+
+    redirect_to "/profile"
+  end
+
+  def destroy
+    UserListingService.delete_user_listing(@current_user[:user_id], params[:listing_id])
+
+    redirect_to "/profile"
+  end
 end
